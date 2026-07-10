cdf1b18ef70ed30f317576603b0aa6d852249284
Dylan <hoangdungpham0703@gmail.com>
2026-07-10 20:31:36 +0300

    Add neovim tokyonight and autosave config

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_augroup("AutoSave", { clear = true })
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  group = "AutoSave",
+  pattern = "*",
+  callback = function()
+    if vim.bo.modified and vim.bo.buftype == "" then
+      vim.cmd("silent! update")
+    end
+  end,
+})
