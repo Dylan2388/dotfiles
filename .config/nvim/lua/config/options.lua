1486d0b7b169bf4e2bf4fffc976b20378118dcd3
Hoang Dung Pham Nguyen <hoang.dung.pham.nguyen@ericsson.com>
2026-07-13 14:42:48 +0000

    Update lazyvim config and kiro AGENTS.md

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,11 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      clipboard = "unnamedplus",
-    },
+vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
   },
 }
